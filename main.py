2ba4dd6f900c6e56e0dc6809c6d0d7c868d5eb85
polinochkak <polinabuharina73@gmail.com>
2022-04-16 16:38:21 +0700

    fin

--- main.py
+++ main.py
@@ -1,44 +1,62 @@
 import cv2
 import mediapipe as mp
+import time
 
-cap = cv2.VideoCapture(0)  # подключение к web-камере
-mp_Hands = mp.solutions.hands  # хотим распознать руки
+cap = cv2.VideoCapture(0)  # Подключение к web-камере 
+mp_Hands = mp.solutions.hands  # говорим, что хотим распознать руки
 hands = mp_Hands.Hands(max_num_hands = 2)  # характеристики для распознавания
 mpDraw = mp.solutions.drawing_utils  # инициализация утилит рисования
 
-fingers_coord = [(8, 6), (12, 10), (16, 14), (20, 18)]  # координаты ключевых точек на руке кроме большого пальца
-thumb_coord = (4, 2)  # координаты ключевых точек большого пальца
+fingers_coord = [(8, 6), (12, 10), (16, 14), (20, 18)]  # координаты ключевых точек на руке, кроме большого пальца
+thumb_coord = (4, 3) # координаты ключевых точек для большого пальца
 
 while cap.isOpened():  # пока камера "работает"
-    success, image = cap.read()  # получаем кадр с камер
-    if not success:  # если не удалось получить кадр с камеры
+    success, image = cap.read()  # получаем кадр с камеры
+    if not success: # если не удалось получить кадр с камеры
         print('Не удалось получить кадр с web-камеры')
         continue  # переход к ближайшему циклу (while 12 строчка)
-    image = cv2.flip(image, 1)  # зеркально отображаем изображение
-    RGB_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # BGR -> RGB
+    prevTime = time.time()
+    image = cv2.flip(image, 1)   # зеркально отражаем изображение
+    RGB_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  #BGR -> RGB изображение
     results = hands.process(RGB_image)  # ищем руки на изображении
-    multiLandMarks = results.multi_hand_landmarks  # извлекаем список найденных рук
+    multiLandMarks = results.multi_hand_landmarks  # Извлекаем список найденных рук
     
-    if multiLandMarks:  # если, руки найдены
+    if multiLandMarks: # если руки найдены
         upCount = 0
-        for idx, handLms in enumerate(multiLandMarks):  # перебираем найденные руки
+        for idx, handLms in enumerate(multiLandMarks): # перебираем найденные руки
             lbl = results.multi_handedness[idx].classification[0].label
             print(lbl)
 
-            mpDraw.draw_landmarks(image, handLms, mp_Hands.HAND_CONNECTIONS)  # рисуем ключевые точки
-            fingerList = []
-            for idx, lm in enumerate(handLms):  # перебираем ключевые точки на руке
+            mpDraw.draw_landmarks(image, handLms, mp_Hands.HAND_CONNECTIONS)
+            fingersList = [] 
+            #ВНИМАНИЕ!!!
+            for lm in handLms.landmark: # перебираем ключевые точки на руке
                 h, w, c = image.shape
                 cx, cy = int(lm.x * w), int(lm.y * h)
-                fingerList.append((cx, cy))
+                fingersList.append((cx, cy))
             for coordinate in fingers_coord:
-                if fingerList[coordinate[0]][1] < fingerList[coordinate[1]][1]:
+                if fingersList[coordinate[0]][1] < fingersList[coordinate[1]][1]:
                     upCount += 1
-        print(upCount)
-    
+            side = 'left'
+            if fingersList[5][0] > fingersList[17][0]:
+                side = 'right'
 
+            if side == 'left':
+                if fingersList[thumb_coord[0]][0] < fingersList[thumb_coord[1]][0]:
+                    upCount += 1
+            else:
+                if fingersList[thumb_coord[0]][0] > fingersList[thumb_coord[1]][0]:
+                    upCount += 1
+
+        cv2.putText(image, lbl, (20, 140), cv2.FONT_HERSHEY_COMPLEX, 1, (70, 160, 46), 2)  #доработать с 2 руками
+        cv2.putText(image, str(upCount), (30, 100), cv2.FONT_HERSHEY_COMPLEX, 4, (10, 60, 246), 2)
+        print(upCount)
+
+    currentTime = time.time()
+    fps = 1 / (currentTime - prevTime)
+    cv2.putText(image, f'FPS: {fps}', (10, 475), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 0), 1)
     cv2.imshow('web-cam', image)
-    if cv2.waitKey(1) & 0xFF == 27:  # ожидаем нажатие клавиши ESC
+    if cv2.waitKey(1) & 0xFF == 27: # ожидаем нажание клавиши ESC
         break
 
 cap.release()
